cb017434fa0504bf208c73cd8988e53cc3795ba0
Neethupp-n <neethupp753@gamil.com>
2019-03-21 20:09:05 +0530

    commited

---
+++ sum.sh
@@ -0,0 +1,9 @@
+echo "Enter the first number : "
+    read num1
+    echo "Enter the second number : "
+    read num2
+    sum=`expr $num1 + $num2`
+    echo "sum of two value is $sum"
+~                                                                               
+~                                                                               
+~                             
